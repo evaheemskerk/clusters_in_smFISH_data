8ea92b4df0dd0069432da01c397abcbb798c3881
Eva Heemskerk <165023519+evaheemskerk@users.noreply.github.com>
2025-05-07 22:04:25 +0200

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-##preprocess particles
-
-
-def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
-    #cluster_data = pd.read_excel(cluster_data)
+def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm, cytokine_analysis):
     df = pd.read_csv(file_name_spots)
     if cytoplasm == True:
         df = df[df['nuclei_mask'] == 0]
     if nucleus == True:
         df = df[df['nuclei_mask'] > 0]
-    # df = df[df['cytokine'] == 'IFN']
+
+    if cytokine_analysis == 'TNF':
+        df = df[df['cytokine'] == 'TNF']
+    if cytokine_analysis == 'IFN':
+        df = df[df['cytokine'] == 'IFN']
 
     # Part 1
     #find general_data
@@ -33,7 +33,6 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
     cytokine_type = pd.merge(cytokine_type, general_data, on=['Timepoint', 'Type'], suffixes=('_clustered', '_all'))
     cytokine_type['percentage'] = cytokine_type['N_clustered_cells']/cytokine_type['N_cells']
     cytokine_type = cytokine_type.sort_values(by=['Type', 'Timepoint'])
-    #cytokine_type['cytokine'] = cytokine_type['Cytokine'].apply(lambda x: ', '.join(x) if isinstance(x, (list, np.ndarray)) else str(x))
     cytokine_type['cytokine'] = cytokine_type['Cytokine'].apply(lambda x: ', '.join(x.replace("'", "").split()) if isinstance(x, str) else str(x))
     cytokine_type['cytokine'] = cytokine_type['cytokine'].apply(lambda x: x.strip("[]").replace("', '", ", ") if isinstance(x, str) else str(x))
 
@@ -61,7 +60,6 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
 
     for timepoint in cytokine_Nclusters['Timepoint'].unique():
         data = cytokine_Nclusters[cytokine_Nclusters['Timepoint'] == timepoint]
-        #sns.barplot(data = data, x = 'N_clusters', y = 'percentage', hue = 'Type')
         sns.pointplot(data=data, x='N_clusters', y='percentage', hue='Type', dodge=True)
         plt.title(f'How many clusters per cell? Timepoint: {timepoint}')
         plt.show()
@@ -71,12 +69,13 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
     g = sns.relplot(data = cytokine_Nclusters, kind ='line', x = 'Timepoint', y = 'N_clusters', hue = 'Type')
     g.fig.subplots_adjust(top=0.9)  # Extra ruimte voor de titel
     g.fig.suptitle(f'Numbers of clusters per cell per timepoint', fontsize=14)
-    # Zet de legenda rechts buiten de plot
-    g._legend.set_bbox_to_anchor((1.0, 0.8))  # (x, y) positie buiten de plot
-    g._legend.set_title("Type")  # Optioneel: titel van de legenda
+    g.set_ylabels(label = 'Average number of clusters')
+    g.set(ylim=(0, 25))
+    g._legend.set_bbox_to_anchor((1.0, 0.8)) 
+    g._legend.set_title("Type")  
     plt.show()
 
-    ### N_particles per cluster
+    ## N_particles per cluster
     for timepoint in cluster_data['Timepoint'].unique():
         data = cluster_data[cluster_data['Timepoint'] == timepoint]
         sns.histplot(data=data, x='N_particles_per_cluster', binwidth = 1, hue = 'Type', multiple= 'dodge')
@@ -84,10 +83,12 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
         plt.xlim(0,40)
         plt.show()
 
-    #cluster_data = cluster_data[cluster_data['Cytokine'] ==  "['IFN' 'TNF']"]
+    ###Number of particles per cluster
     g = sns.relplot(data=cluster_data, kind='line', x='Timepoint', y='N_particles_per_cluster', hue='Type')
     g.fig.subplots_adjust(top=0.9)
-    g.fig.suptitle('Numbers of particles per cluster per timepoint', fontsize=14)
+    g.fig.suptitle('Average number of particles per cluster per timepoint', fontsize=14)
+    g.set_ylabels(label = 'Number of particles per cluster')
+    g.set(ylim=(2.5, 7))
     g._legend.set_bbox_to_anchor((1.0, 0.8))
     g._legend.set_title("Type")
     plt.show()
@@ -99,7 +100,11 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
         df = df[df['nuclei_mask'] == 0]
     if nucleus == True:
         df = df[df['nuclei_mask'] > 0]
-    # df = df[df['cytokine'] == 'IFN']
+
+    if cytokine_analysis == 'TNF':
+        df = df[df['cytokine'] == 'TNF']
+    if cytokine_analysis == 'IFN':
+        df = df[df['cytokine'] == 'IFN']
 
     df = df.groupby('ID')['cytokine'].unique().reset_index()
     df['cytokine'] = df['cytokine'].apply(lambda x: ', '.join(x) if isinstance(x, (list, np.ndarray)) else str(x))
@@ -113,7 +118,7 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
     producer_data = producer_data.sort_values(by=['Type', 'Timepoint'], ascending=[True, True])
     producer_data['Timepoint'] = pd.Categorical(
         producer_data['Timepoint'],
-        categories=sorted(producer_data['Timepoint'].unique()),  # Of geef een eigen volgorde op
+        categories=sorted(producer_data['Timepoint'].unique()),
         ordered=True
     )
     print(producer_data)
@@ -131,8 +136,11 @@ def plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm):
 
 if __name__ == '__main__':
     file_name_spots = "C:/Users/Eva Heemskerk/Documents/Delft/TU Delft/MEP/4. Results/DATA/Spots_with_localization.csv"
-    cluster_data = 'particles_data_cytoplasm_minsamp3.xlsx'
+    cluster_data = pd.read_excel("C:/Users/Eva Heemskerk/PycharmProjects/MEP/particles_data_nucleus_both.xlsx")
     nucleus = False
     cytoplasm = True
 
-    plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm)
+    # Fill in BOTH if interested in both TNF and IFN, or fill in 'TNF' or 'IFN' when interested in a single analysis
+    cytokine_analysis = 'BOTH'
+
+    plot_dbscan(file_name_spots, cluster_data, nucleus, cytoplasm, cytokine_analysis)
